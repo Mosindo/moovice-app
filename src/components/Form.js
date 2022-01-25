dd1563f5fba5227d72b89d99f2ff388394c042bb
mosindo <mosindo.mbilinyama@hotmail.fr>
2022-01-25 15:26:01 +0100

    add component card

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import Card from './Card';
 
 const Form = () => {
 	const [moviesData, setMoviesData] = useState([]);
@@ -29,7 +30,7 @@ const Form = () => {
 			</div>
 			<div className="result">
 				{moviesData.map((movie) => (
-					<h2> {movie.title}</h2>
+					<Card key={movie.id} movie={movie} />
 				))}
 			</div>
 		</div>
